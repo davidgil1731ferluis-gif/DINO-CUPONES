ed0ca7a7e4881eddf4de55cf2ddd273ae810711a
davidgil1731ferluis-gif <davidgil1731ferluis@gmail.com>
2026-09-20 13:19:13 -0500

    Version DinoDuo Firebase fix

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ import {
   identifyPushUser,
   clearPushUser,
   onForegroundMessage
-} from './firebase-service.js?v=20260920-6';
+} from './firebase-service.js?v=20260920-7';
 
 const $ = (s) => document.querySelector(s);
 const $$ = (s) => [...document.querySelectorAll(s)];
