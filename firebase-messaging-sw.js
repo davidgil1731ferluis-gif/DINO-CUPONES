a2a2efa99de2841ee5cc97c830c09be71c31a69e
davidgil1731ferluis-gif <davidgil1731ferluis@gmail.com>
2026-09-19 19:19:38 -0500

    Add service worker scaffold

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('notificationclick',event=>{event.notification.close();event.waitUntil(clients.openWindow(event.notification.data?.url||'./'));});
