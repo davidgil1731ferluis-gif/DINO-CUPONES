d223f930c54f529901f023e9c6dd7607aac30ef9
davidgil1731ferluis-gif <davidgil1731ferluis@gmail.com>
2026-09-19 19:19:32 -0500

    Add Firebase configuration placeholder

---
+++ js/firebase-config.js
@@ -0,0 +1,2 @@
+export const firebaseConfig={apiKey:"TU_API_KEY",authDomain:"TU_PROJECT_ID.firebaseapp.com",projectId:"TU_PROJECT_ID",storageBucket:"TU_PROJECT_ID.firebasestorage.app",messagingSenderId:"TU_MESSAGING_SENDER_ID",appId:"TU_APP_ID"};
+export const vapidKey="TU_VAPID_KEY";
