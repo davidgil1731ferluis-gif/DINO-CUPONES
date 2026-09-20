7c9026040052057c23d96ccae50054945005e102
davidgil1731ferluis-gif <davidgil1731ferluis@gmail.com>
2026-09-20 14:07:36 -0500

    Refresh installed PWA for push self-test

--- js/intro-bootstrap.js
+++ js/intro-bootstrap.js
@@ -100,7 +100,7 @@
     revealLetter();
   }
 
-  const APP_BUILD = '20260920-12';
+  const APP_BUILD = '20260920-13';
   let updateReloading = false;
 
   async function checkForUpdate(forceReload = false) {
