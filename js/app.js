4a3a5a26dc9e398f6840e85b065cc4e5ab47100e
davidgil1731ferluis-gif <davidgil1731ferluis@gmail.com>
2026-09-20 13:46:58 -0500

    Version message and PWA updater fixes

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ import {
   identifyPushUser,
   clearPushUser,
   onForegroundMessage
-} from './firebase-service.js?v=20260920-10';
+} from './firebase-service.js?v=20260920-11';
 
 const $ = (s) => document.querySelector(s);
 const $$ = (s) => [...document.querySelectorAll(s)];
