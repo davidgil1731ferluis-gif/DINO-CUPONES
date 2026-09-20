95f0b636fc59ac2032cd9584fca107edb52adfd8
davidgil1731ferluis-gif <davidgil1731ferluis@gmail.com>
2026-09-20 13:27:58 -0500

    Version DinoDuo recipient UX

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ import {
   identifyPushUser,
   clearPushUser,
   onForegroundMessage
-} from './firebase-service.js?v=20260920-7';
+} from './firebase-service.js?v=20260920-8';
 
 const $ = (s) => document.querySelector(s);
 const $$ = (s) => [...document.querySelectorAll(s)];
