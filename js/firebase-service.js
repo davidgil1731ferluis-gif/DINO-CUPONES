145e15cd2b87a86e0fd3e200d132a95eb385eaa9
davidgil1731ferluis-gif <davidgil1731ferluis@gmail.com>
2026-09-20 14:07:28 -0500

    Version OneSignal integration config import

--- js/firebase-service.js
+++ js/firebase-service.js
@@ -4,7 +4,7 @@ import {
   cloudinaryReady,
   oneSignalReady,
   workerReady
-} from './integrations-config.js';
+} from './integrations-config.js?v=20260920-13';
 
 const V = '12.19.0';
 const configured = firebaseConfig.apiKey && !firebaseConfig.apiKey.startsWith('TU_');
