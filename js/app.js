3f62dcd68b23625660032b2e06c7c09cb9956b1e
davidgil1731ferluis-gif <davidgil1731ferluis@gmail.com>
2026-09-20 12:52:31 -0500

    Refresh Firebase service module version

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ import {
   identifyPushUser,
   clearPushUser,
   onForegroundMessage
-} from './firebase-service.js?v=20260920-4';
+} from './firebase-service.js?v=20260920-5';
 
 const $ = (s) => document.querySelector(s);
 const $$ = (s) => [...document.querySelectorAll(s)];
